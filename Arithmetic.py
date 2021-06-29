2fdc46fd6be580a775ce0dc1f0e549c1e1173ec0
Abderrahman <abderrahman.a@outlook.com>
2021-06-29 14:20:50 +0200

    matte er matte

---
+++ Arithmetic.py
@@ -0,0 +1,19 @@
+print (10/3) #for interger use "//" for modulous(remaider of division) use "%"
+print (10 ** 3) # used for 10 in the power of 3 eller 10 opphøyd i tredje.
+
+x = 10
+
+x = x + 3 # incraments a number
+print(x)
+
+x = 10
+x -= 3 #augmented assignment operator.
+
+x = 10 + 3 * 2 ** 2 #operator precedence (multiply first then add)
+
+x = 2.9
+print(round(x)) #gives the rounded up number
+print(abs(-2.9)) #alvays returns a positive number
+
+import math #this imports the matc module/gives chatsheats for math code
+# to bring out the math cheatsheat type ".math" and chose formula you are looking for(https://docs.python.org/3/library/math.html)
